007c18ee07eec5ecd030780e6558ec839305653c
Rohitth Bandaru <rohitthbandaru@gmail.com>
2022-08-23 21:07:17 +0530

    created seperate pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import About  from "./components/About";
+import Education from "./components/Education"
+
+
+
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+    <About></About>
+    <Education></Education>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/CodingProfile.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CodingProfile = () => {
+  return <div>CodingProfile</div>;
+};
+
+export default CodingProfile;
