f13a0c802f0a1fcf7128590eceef6bb51fa484bc
Rohitth Bandaru <rohitthbandaru@gmail.com>
2022-08-25 00:02:54 +0530

    added navbar without links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React, { Component } from "react";
-import About  from "./components/About";
-import Education from "./components/Education"
-
-
-
+import React from "react";
+import About from "./components/About";
+import Education from "./components/Education";
+import CodingProfile from "./components/CodingProfile";
+import Contact from "./components/Contact";
+import Skillset from "./components/Skillset";
+import SocialMedia from "./components/SocialMedia";
+import Resume from "./components/Resume";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-    <About></About>
-    <Education></Education>
+    <div className="App" class="font-poppins bg-black text-white">
+      <div>
+        <Navbar></Navbar>
+      </div>
+      <div>
+        <About></About>
+      </div>
+      <div>
+        <Education></Education>
+      </div>
+      <div>
+        <Skillset></Skillset>
+      </div>
+      <div>
+        <CodingProfile></CodingProfile>
+      </div>
+      <div>
+        <Resume></Resume>
+      </div>
+      <div>
+        <Contact></Contact>
+      </div>
+      <div>
+        <SocialMedia></SocialMedia>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div class="bg-zinc-900 w-full h-14 text-white flex flex-column justify-around items-center">
+      <div>
+        <a href="">ABOUT</a>
+      </div>
+      <div>
+        <a href="">EDUCATION</a>
+      </div>
+      <div>
+        <a href="">CODING PROFILE</a>
+      </div>
+      <div>
+        <a href="">SKILL SET</a>
+      </div>
+      <div>
+        <a href="">CONTACT</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
