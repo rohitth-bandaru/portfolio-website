0c39110a4aafa16bb342cf146bc9316f25441a01
Rohitth Bandaru <rohitthbandaru@gmail.com>
2022-08-25 01:24:50 +0530

    Education page added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div class="w-full h-screen flex items-center">
+      <div class="text-2xl w-1/2 p-16 ">
+        I am 
+        <div class="text-red-500">Rohitth Bandaru</div> Highly motivated BITSian intreseted in the software
+        development field
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import logo from "../ASSETS/BITS_Pilani-Logo.svg.png";
 const Education = () => {
-  return <div>Education</div>;
+  return (
+    <div class="w-full h-screen  flex flex-row items-center justify-around">
+      <div>
+        <img class="w-80 h-80" src={logo} />
+      </div>
+      <div class="text-2xl grid gap-1">
+        <div>
+          <div>B.E Mechanical Engineering</div>
+          <div class="text-sm">
+            ( Birla Institute of Technology and Science, Hyderabad )
+          </div>
+        </div>
+        <div><span class="text-slate-500">Mechanical Engineer</span> by Degree</div>
+        <div><span class="text-red-500">Software Developer</span> by Passion</div>
+      </div>
+    </div>
+  );
 };
 
 export default Education;
