84622b2fdfe1e10b598cc253850cf485d40d17e3
Rohitth Bandaru <rohitthbandaru@gmail.com>
2022-08-27 17:31:17 +0530

    updated final results of google kickstart round E

--- src/components/CodingProfile.jsx
+++ src/components/CodingProfile.jsx
@@ -6,14 +6,14 @@ const CodingProfile = () => {
       <div class="w-full h-14"></div>
       <div class="w-full h-96  text-2xl flex flex-col justify-center items-center justify-around">
         <div class="">
-          Secured <span class="text-red-500">1037</span> rank in Google
-          Kickstart Round E 2022
+          Secured <span class="text-red-500">998</span> rank in Google Kickstart
+          Round E 2022
         </div>
         <div class="">
           currently rated at <span class="text-red-500">1622</span> in leetcode
         </div>
         <div class="">
-          <span class="text-red-500">998</span> rating in codeforces
+          <span class="text-red-500">1318</span> rating in codeforces
         </div>
         <div></div>
       </div>
